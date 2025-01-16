2cd27214c6348c6bb3c0bbdddf2e925a5531d950
nesquikx014 <n.eroglux014@gmail.com>
2025-01-16 13:09:02 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("js--menu");
+const navigation = document.getElementById("js--nav");
+
+button.onclick = function() {
+    navigation.style.visibility = "visible";
+}
